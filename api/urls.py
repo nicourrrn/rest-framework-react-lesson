3134cce30c7939317deba312b52f1cf1de5ae3d0
Vadym <s57111702@gmail.com>
2023-06-07 23:12:15 +0300

    Class based view

--- api/urls.py
+++ api/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 
 urlpatterns = [
-    path('project/', views.project_list),
-    path('project/<int:pk>', views.project_detail),
+    path('project/', views.ProjectList.as_view()),
+    path('project/<int:pk>', views.ProjectDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -1,37 +1,43 @@
+from django.http import Http404
 
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.request import Request
+from rest_framework.views import APIView
 
 from api.models import Project
 from api.serializers import ProjectSerializser
 
-@api_view(["GET", "PUT"])
-def project_list(request: Request, format=None):
-    if request.method == "GET":
+class ProjectList(APIView):
+
+    def get(self, request: Request, format=None):
         projects = Project.objects.all()
         serialized = ProjectSerializser(projects, many=True)
         return Response(serialized.data)
-    elif request.method == "PUT":
+
+    def post(self, request: Request, format=None):
         serialized = ProjectSerializser(data=request.data)
         if serialized.is_valid():
             serialized.save()
             return Response(serialized.data, status=status.HTTP_201_CREATED)
         return Response(serialized.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class ProjectDetail(APIView):
 
-@api_view(["GET", "DELETE"])
-def project_detail(request: Request, pk: int, format=None):
-    try:
-        proj = Project.objects.get(pk=pk)
-    except Project.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    def get_object(self, pk: int) -> Project:
+        try:
+            return Project.objects.get(pk=pk)
+        except Project.DoesNotExist:
+            raise Http404
 
-    match request.method:
-        case "GET":
-            serialized = ProjectSerializser(proj)
-            return Response(serialized.data)
-        case "DELETE":
-            proj.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
+    def get(self, request: Request, pk: int, format=None):
+        project = self.get_object(pk)
+        serialized = ProjectSerializser(project)
+        return Response(serialized.data)
+
+    def post(self, request: Request, pk:int, format=None):
+        project = self.get_object(pk)
+        project.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
