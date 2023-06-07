c2dd06cd93330fff9cdf2a09532b66243650adfd
Vadym <s57111702@gmail.com>
2023-06-07 23:20:46 +0300

    Use combined classes

--- api/views.py
+++ api/views.py
@@ -1,35 +1,16 @@
 
-from rest_framework import mixins, generics
+from rest_framework import generics
 
 from api.models import Project
 from api.serializers import ProjectSerializser
 
-
-class ProjectList(mixins.ListModelMixin, mixins.CreateModelMixin,
-                  generics.GenericAPIView):
-
+class ProjectList(generics.ListCreateAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializser
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
 
 
-
-class ProjectDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin, generics.GenericAPIView):
-
+class ProjectDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializser
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-    
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
